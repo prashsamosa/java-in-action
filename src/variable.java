cee3cd0e3e0bb732696977b4bfded1ad4006380f
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-06-10 06:36:46 +0530

    variable

---
+++ src/variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 5;
+        String b = "Hello";
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

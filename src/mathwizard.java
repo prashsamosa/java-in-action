7f1273a9f1118627e64d804a1c54a9602feb92b7
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-06-15 15:01:02 +0530

    collection framework

--- src/mathwizard.java
+++ src/mathwizard.java
@@ -1,3 +1,22 @@
 public class mathwizard {
+    public static void main(String[] args) {
+        int yourSalary = 50000; // Example salary
+        int bonus = 5000; // Example bonus
+        int totalSalary = yourSalary + bonus; // Calculate total salary
+        System.out.println("Your total salary is: " + totalSalary); // Output the total salary
+        int a= 3;
+        double b = 3.14;
+        double c = a + b;
+        System.out.println("The sum of a and b is: " + c); // Output the sum of a and b
+
+        int e = 5;
+        int g = e++;
+        System.out.println(e);
+        System.out.println(g);
+
+
+
+
+    }
 
 }
